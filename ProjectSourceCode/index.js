3280b0c38a4195dd352f8cf18da6a6844bb8c2a6
LuminMCB <tmschmitz@cox.net>
2024-11-03 09:46:18 -0700

    Initialized project with Docker and Express.js
    - added docker-compose.yaml to setup PostgreSQL and Node.js
    - created index.js to start server with temporary welcome message
    - update .gitignore to exlude .env, node_modules, etc.

---
+++ ProjectSourceCode/index.js
@@ -0,0 +1,70 @@
+// ------------------------------------
+//         Import Dependencies
+// ------------------------------------
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcryptjs'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+// ------------------------------------
+//           Connect to DB
+// ------------------------------------
+
+const dbConfig = {
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+// ------------------------------------
+//            App Settings
+// ------------------------------------
+
+app.use( // initialize session variables
+    session({
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+  
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+
+// ------------------------------------
+//             API Routes
+// ------------------------------------
+
+app.get('/', (req, res) => { // temporary route that just shows a message
+    res.send('<h1>This is Project-ActiveU!</h1>');
+});
+  
+// ------------------------------------
+//             Start Server
+// ------------------------------------
+app.listen(3000);
+console.log('Server is listening on port 3000');
