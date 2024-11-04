e671ef737fef6665f580b6add2015d5d3cc0dc9b
Jose Martinez <jose.martinez-barreat@colorado.edu>
2024-11-04 16:03:30 -0700

    foundation of database, early start on user friendship functionality.

---
+++ ProjectSourceCode/init_data/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    email VARCHAR(200) NOT NULL,
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60) NOT NULL
+);
+
+/*
+CREATE TABLE friendships (
+    user1 VARCHAR(50) NOT NULL,
+    user2 VARCHAR(50) NOT NULL,
+    status ENUM('pending', 'accepted') DEFAULT 'pending',
+    PRIMARY KEY (user1, user2),
+    FOREIGN KEY (user1) REFERENCES users(username) ON DELETE CASCADE,
+    FOREIGN KEY (user2) REFERENCES users(username) ON DELETE CASCADE
+);
+*/
+
+-- worry about friend stuff later. above referenced from lab 6
