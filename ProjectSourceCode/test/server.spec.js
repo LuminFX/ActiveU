aed0b8ff7413cc69be8574675078c057d19ef2aa
Jose Martinez <jose.martinez-barreat@colorado.edu>
2024-11-07 10:53:33 -0700

    register positive and negative test case. api register post fixed

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -1,6 +1,6 @@
 // ********************** Initialize server **********************************
 
-const server = require('../index.js'); 
+const server = require('../index.js');
 
 // ********************** Import Libraries ***********************************
 
@@ -8,7 +8,7 @@ const chai = require('chai'); // Chai HTTP provides an interface for live integr
 const chaiHttp = require('chai-http');
 chai.should();
 chai.use(chaiHttp);
-const {assert, expect} = chai;
+const { assert, expect } = chai;
 
 // ********************** DEFAULT WELCOME TESTCASE ****************************
 
@@ -22,6 +22,37 @@ describe('Server!', () => {
         expect(res).to.have.status(200);
         expect(res.body.status).to.equals('success');
         assert.strictEqual(res.body.message, 'Welcome!');
+        done();
+      });
+  });
+});
+
+// ********************** PART B TEST CASES ****************************
+
+describe('Testing Registering', () => {
+
+  // verify whether its getting properly inserted into the users table. 
+  it('positive : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({ password: 'mypassword', email: 'myemail@gmail.com' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/)
+        done();
+      });
+  });
+
+  // testing whether your API recongizes incorrect POSTs and responds with the appropriate error message.
+  it('Negative : /register. Checking invalid password', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({ password: 'a', email: 20 })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.equals('Password must be at least 5 characters long.');
         done();
       });
   });
