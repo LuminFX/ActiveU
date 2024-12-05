dcae4481ddab82bf4f7b7ae361d91a3590ed45ee
LuminMCB <tmschmitz@cox.net>
2024-12-04 19:44:49 -0700

    Add delete.sql for easy database deconstruction Update gitignore to exclude init_db.sh

---
+++ ProjectSourceCode/delete_data/delete.sql
@@ -0,0 +1,11 @@
+-- WORKOUT STUFF
+DROP TABLE IF EXISTS workout_reactions;
+DROP TABLE IF EXISTS exercises;
+DROP TABLE IF EXISTS workouts;
+
+-- USER STUFF
+DROP TABLE IF EXISTS friendships;
+DROP TABLE IF EXISTS users;
+
+-- Drop type
+DROP TYPE IF EXISTS friendship_status;
